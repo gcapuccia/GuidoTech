f3f92d0049eb90ae1140a6792d64644adbab9a5b
gcapuccia <guidoca_94@hotmail.com>
2025-10-29 19:21:04 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// Configuración de Tailwind
+tailwind.config = {
+    darkMode: "class",
+    theme: {
+        extend: {
+            colors: {
+                primary: "#1173d4",
+                "background-light": "#f6f7f8",
+                "background-dark": "#101922",
+            },
+            fontFamily: {
+                display: ["Space Grotesk", "sans-serif"],
+            },
+            borderRadius: {
+                DEFAULT: "0.25rem",
+                lg: "0.5rem",
+                xl: "0.75rem",
+                full: "9999px",
+            },
+        },
+    },
+};
